2404735b9d3836c803c84063de1ab0a601495245
rojasmichael <rojas.michael@hotmail.com>
2015-11-14 23:23:04 +0000

    nitrous-backup at Sat Nov 14 23:23:04 UTC 2015

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,20 @@
-# code your #position_taken? method here!
+def taken?
+    owner.nil?
+  end
+
+  def empty?
+    !taken?
+  end
+end
+
+def draw?
+  @board.all &:taken?
+end
+
+def display_board
+  @board.each.with_index(1) |field, idx|
+    print "#{field.owner || idx} | "
+    print "\n-----------\n" if idx % 3 == 0
+  end
+end
+
